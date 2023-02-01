bebd9f25ecd0f462c68a0864f46f56eb4f06332e
Aryan Mankame <aryan672002@gmail.com>
2023-02-02 00:16:46 +0530

    hexagons

---
+++ hexagons.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int t; cin >> t;
+  long long int ans = 1;
+  ans += (long long int)3*t*t;
+  ans += (long long int)3*t;
+  cout << ans << endl;
+}
